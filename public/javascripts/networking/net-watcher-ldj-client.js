410890c96e3ac0236e0b7c63a7ed621b1bece992
garrotin <streborc@gmail.com>
2018-03-08 16:20:46 -0500

    unstaged files

--- public/javascripts/networking/net-watcher-ldj-client.js
+++ public/javascripts/networking/net-watcher-ldj-client.js
@@ -1,3 +1,18 @@
 /**
  * Created by chadroberts on 3/8/18.
  */
+'use strict';
+const netClient = require('net').connect({port:60300});
+const ldjClient = require('./lib/ldj-client').connect(netClient);
+
+ldjClient.on('message', message => {
+   if(message.type === 'watching'){
+        console.log(`Now watching: ${message.file}`);
+    }else if (message.type === 'changed'){
+       console.log(`File changed: ${new Date(message.timestamp)}`);
+    }else{
+        throw Error(`Unrecognized message type: ${message.type}`);
+    }
+
+
+});
--- public/javascripts/networking/test/ldj-client-test.js
+++ public/javascripts/networking/test/ldj-client-test.js
@@ -1,3 +1,38 @@
 /**
  * Created by chadroberts on 3/8/18.
  */
+'use strict';
+const assert = require('assert');
+const EventEmitter = require('events').EventEmitter;
+const  LDJClient = require('../lib/ldj-client');
+
+describe('LDJClient', () => {
+   let stream = null;
+   let client = null;
+
+   beforeEach(() => {
+        stream = new EventEmitter();
+        client = new LDJClient(stream);
+   });
+
+    it('should emit a message event from a single data event', done => {
+        client.on('message', message => {
+        assert.deepEqual(message, {foo: 'bar'});
+    done();
+    });
+    stream.emit('data', '{"foo":"bar"}\n');
+    });
+
+    it('should emit a message event from split data events', done => {
+        client.on('message', message => {
+        assert.deepEqual(message, {foo: 'bar'});
+        done();
+    });
+    stream.emit('data', '{"foo":');
+        process.nextTick(() => stream.emit('data', '"bar"}\n'));
+    });
+
+    it('should finish within 5 seconds', done =>{
+       setTimeout(done, 4500) ; //call done after 4.5 seconds
+    }).clearTimeout(5000);
+});
